3917f9b805f6686967d9e9bee6d6ae9d8f398b04
ElviraDev <elvirates@gmail.com>
2018-10-08 17:55:25 +0300

    add logger

--- src/main/java/ru/innopolis/stc/controller/StartPage.java
+++ src/main/java/ru/innopolis/stc/controller/StartPage.java
@@ -1,5 +1,8 @@
 package ru.innopolis.stc.controller;
 
+import org.apache.log4j.Logger;
+import sun.applet.Main;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -7,9 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class StartPage extends HttpServlet {
+    private final static Logger LOGGER = Logger.getLogger(StartPage.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        LOGGER.info("Call do get");
         req.getRequestDispatcher("WEB-INF/pages/start_page.jsp").forward(req, resp);
-        System.out.println("fffff");
-    }
+        }
 }
