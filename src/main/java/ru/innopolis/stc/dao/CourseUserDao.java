adde310877aae6e323ca773ffe1332b36108c9df
ElviraDev <elvirates@gmail.com>
2018-10-11 13:35:49 +0300

    add try with resourse for resultSet

--- src/main/java/ru/innopolis/stc/dao/CourseUserDao.java
+++ src/main/java/ru/innopolis/stc/dao/CourseUserDao.java
@@ -1,12 +1,13 @@
 package ru.innopolis.stc.dao;
 
 import ru.innopolis.stc.pojo.Course;
+import ru.innopolis.stc.pojo.CourseUser;
 import ru.innopolis.stc.pojo.User;
 
 import java.util.List;
 
 public interface CourseUserDao {
-    List<User> findUsers(User user);
+    List<CourseUser> findCourses(User user);
 
-    List<Course> findCourses(Course course);
+    public List<CourseUser> findUsers(Course course);
 }
--- src/main/java/ru/innopolis/stc/daosql/CourseUserDaoImpl.java
+++ src/main/java/ru/innopolis/stc/daosql/CourseUserDaoImpl.java
@@ -5,6 +5,7 @@ import ru.innopolis.stc.controller.StartPage;
 import ru.innopolis.stc.dao.CourseUserDao;
 import ru.innopolis.stc.db.connectionPool.DatabaseConnectionPool;
 import ru.innopolis.stc.pojo.Course;
+import ru.innopolis.stc.pojo.CourseUser;
 import ru.innopolis.stc.pojo.User;
 
 import java.sql.Connection;
@@ -18,21 +19,18 @@ public class CourseUserDaoImpl implements CourseUserDao {
     DatabaseConnectionPool databaseConnectionPool = new DatabaseConnectionPool("jdbc:postgresql://localhost:5432/allcourse", "postgres", "1234");
     private final static Logger LOGGER = Logger.getLogger(StartPage.class);
     @Override
-    public List<User> findUsers(User user) {
-        List<User> users = null;
+    public List<CourseUser> findUsers(Course course) {
+        List<CourseUser> courseUsers = null;
         try (Connection connection = databaseConnectionPool.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("select u.id, u.role, u.email, u.password, u.firstname, u.secondname from course_user as cu inner join \"user\" as u on cu.user_id = u.id and cu.user_id = ?");
-            preparedStatement.setInt(1, user.getId());
+            PreparedStatement preparedStatement = connection.prepareStatement("select cu.course_id, cu.user_id from course_user as cu inner join course as c on cu.course_id = c.id and cu.course_id = ?");
+            preparedStatement.setInt(1, course.getId());
             try (ResultSet resultSet = preparedStatement.executeQuery();) {
-                users = new ArrayList<>();
+                courseUsers = new ArrayList<>();
                 while (resultSet.next()) {
-                    users.add(new User(
-                            resultSet.getInt(1),
-                            resultSet.getInt(2),
-                            resultSet.getString(3),
-                            resultSet.getString(4),
-                            resultSet.getString(5),
-                            resultSet.getString(6)));
+                    courseUsers.add(new CourseUser(null, null));
+//                            resultSet.getInt(1),
+//                            resultSet.getInt(2));
+                    // TODO: 10.10.2018 use method CourseDao.getID(int id); with check. use method UserDao.getID(int id); with check.
                 }
             }
         } catch (SQLException e) {
@@ -40,24 +38,24 @@ public class CourseUserDaoImpl implements CourseUserDao {
         } catch (ClassNotFoundException e) {
             LOGGER.error(e);
         }
-        return users;
+        return courseUsers;
     }
 
     @Override
-    public List<Course> findCourses(Course course) {
-        List<Course> courses = null;
+    public List<CourseUser> findCourses(User user) {
+        List<CourseUser> courseUsers = null;
         try (Connection connection = databaseConnectionPool.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("select c.id, c.modetationstatus, c.name, c.description, c.teacher_id from course_user as cu inner join course as c on cu.course_id = c.id and cu.course_id = ?");
-            preparedStatement.setInt(1, course.getId());
+            PreparedStatement preparedStatement = connection.prepareStatement("select cu.course_id, cu.user_id from course_user as cu inner join \"user\" as u on cu.user_id = u.id and cu.user_id = ?");
+            preparedStatement.setInt(1, user.getId());
             try (ResultSet resultSet = preparedStatement.executeQuery();) {
-                courses = new ArrayList<>();
+                courseUsers = new ArrayList<>();
                 while (resultSet.next()) {
-                    courses.add(new Course(
-                            resultSet.getInt(1),
-                            resultSet.getBoolean(2),
-                            resultSet.getString(3),
-                            resultSet.getString(4)));
-                    // TODO: 10.10.2018 use method CourseDao.getID(int id); with check. Source from resultSet.getInt(5)
+                    courseUsers.add(new CourseUser(
+                            null, null) // TODO: 11.10.2018 this add getId() from courseDao and add getId() from userDao
+                    );
+                    // TODO: 11.10.2018 this add getId() from courseDao and add getId() from userDao
+//                            resultSet.getInt(1),
+//                            resultSet.getInt(2)));
                 }
             }
         } catch (SQLException e) {
@@ -65,6 +63,6 @@ public class CourseUserDaoImpl implements CourseUserDao {
         } catch (ClassNotFoundException e) {
             LOGGER.error(e);
         }
-        return courses;
+        return courseUsers;
     }
 }
