c91538db37e68b32e2b42289b58f89704ae3f3da
poisondog851 <harisovrinat851@gmail.com>
2018-10-24 19:46:54 +0300

    Разделила страницы создание и редактирование курса. Редактирование курса теперь на странице отображения курса

--- src/main/java/ru/innopolis/stc/controller/LessonController.java
+++ src/main/java/ru/innopolis/stc/controller/LessonController.java
@@ -3,16 +3,17 @@ package ru.innopolis.stc.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.innopolis.stc.bean.Course;
 import ru.innopolis.stc.bean.Lesson;
-import ru.innopolis.stc.bean.Teacher;
 import ru.innopolis.stc.service.ICourseService;
 import ru.innopolis.stc.service.ILessonService;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.constraints.NotBlank;
-import java.util.List;
 
 @Controller
 public class LessonController {
@@ -46,7 +47,7 @@ public class LessonController {
             Lesson lesson = new Lesson(course, name, content, homework);
             lessonService.add(lesson);
         }
-        return "redirect:/course/create/"+courseId;
+        return "redirect:/course/"+courseId;
     }
 
     @PostMapping("lesson/create")
